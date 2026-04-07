1949bd8a03389fe4715ac4c7fe43fe8181cd9cad
mecener <mecener@yandex.ru>
2026-04-07 11:33:55 +0300

    hotfix

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -1,10 +1,12 @@
 export class UserDto {
 	email;
+	username;
 	id;
 	isActivated;
 
-	constructor(model: { email: string; id: number; isActivated: boolean }) {
+	constructor(model: { email: string; username: string; id: number; isActivated: boolean }) {
 		this.email = model.email;
+		this.username = model.username;
 		this.id = model.id;
 		this.isActivated = model.isActivated;
 	}
